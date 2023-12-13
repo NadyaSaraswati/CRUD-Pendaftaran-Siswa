d141dfa889f0ec8e666a9bac42053b61358e271e
NadyaSaraswati <135929497+NadyaSaraswati@users.noreply.github.com>
2023-12-13 16:40:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+	<link rel="stylesheet" href="style.css" />
+	<title>Pendaftaran Siswa Baru | SMK Coding</title>
+</head>
+
+<body>
+	<header>
+		<h3>Pendaftaran Siswa Baru</h3>
+		<h1>SMK Coding</h1>
+	</header>
+	
+	<h3>Menu</h3>
+	<nav>
+		<ul>
+			<li><a href="form-daftar.php">Daftar Baru</a></li>
+			<li><a href="list-siswa.php">Pendaftar</a></li>
+		</ul>
+	</nav>
+	
+	
+	<?php if(isset($_GET['status'])): ?>
+	<p>
+		<?php
+			if($_GET['status'] == 'sukses'){
+				echo "Pendaftaran siswa baru berhasil!";
+			} else {
+				echo "Pendaftaran gagal!";
+			}
+		?>
+	</p>
+	<?php endif; ?>
+	
+	</body>
+</html>
